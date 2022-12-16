65ea9aaa36d739562e2f1982937c382cbff5dbd0
jonathanhaile <jonathan_ha@hotmail.com>
2022-12-16 12:12:11 +0100

    uppdaterat

--- nobel_prize.py
+++ nobel_prize.py
@@ -1,11 +1,4 @@
 import requests
-from test import nobel_data
-
-# Tips: använd sidan nedan för att se vilken data vi får tillbaks och hur apiet fungerar
-# vi använder oss enbart av /nobelPrizes
-# Dokumentation, hjälp samt verktyg för att testa apiet fins här: https://app.swaggerhub.com/apis/NobelMedia/NobelMasterData/2.1
-
-
 
 help_string = """
 Ange ett år och fält
@@ -27,41 +20,33 @@ categori = {"fysik": "phy",
        "medicin": "med"}
 
 
-
-# TODO 10p programmet skall ge en hjälpsam utskrift istället för en krasch om användaren skriver in fel input
-# TODO 15p om användaren inte anger ett område som exempelvis fysik eller kemi så skall den parametern inte skickas med till apiet och vi får då alla priser det året
-
-
-
 def main():
-
     while True:
 
-        # TODO 5p Skriv bara ut hjälptexten en gång när programmet startar inte efter varje gång användaren matat in en fråga
-        #      Förbättra hjälputskriften så att användaren vet vilka fält, exempelvis kemi som finns att välja på
-
-        # TODO 5p Gör så att det finns ett sätt att avsluta programmet, om användaren skriver Q så skall programmet stängas av
-        #      Beskriv i hjälptexten hur man avslutar programmet
-        # TODO 5p Gör så att hjälptexten skrivs ut om användaren skriver h eller H
-
         aaa = input(">")
-        year, field = aaa.split()
-        c = categori[field]
+        if aaa.lower() == "q":
+            break
+        elif aaa.lower() == "h":
+            print(help_string)
+            continue
 
-        c = {"nobelPrizeYear": int(year), "nobelPrizeCategory": c}
+        try:
+            year, field = aaa.split()
+            year = int(year)
+            c = categori[field]
+        except ValueError:
+            print("Ange ett år och fält")
+            continue
+        except KeyError:
+            print("Ange ett år och fält")
+            continue
 
-        res = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params= c).json()
+        params = {"nobelPrizeYear": year, "nobelPrizeCategory": c}
+
+        res = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=params).json()
 
 
-        # TODO 5p  Lägg till någon typ av avskiljare mellan pristagare, exempelvis --------------------------
-
-        # TODO 20p Skriv ut hur mycket pengar varje pristagare fick, tänk på att en del priser delas mellan flera mottagare, skriv ut både i dåtidens pengar och dagens värde
-        #   Skriv ut med tre decimalers precision. exempel 534515.123
-        #   Skapa en funktion som hanterar uträkningen av prispengar och skapa minst ett enhetestest för den funktionen
-        #   Tips, titta på variabeln andel
-        # Feynman fick exempelvis 1/3 av priset i fysik 1965, vilket borde gett ungefär 282000/3 kronor i dåtidens penningvärde
-
-        for nobelPrizes in nobel_data():
+        for person in res["nobelPrizes"]:
             peng = person["prizeAmount"]
             idagpeng = person["prizeAmountAdjusted"]
             print(f"{person['categoryFullName']['se']} prissumma {peng} SEK")
@@ -71,7 +56,6 @@ def main():
                 print(m['knownName']['en'])
                 print(m['motivation']['en'])
                 andel = m['portion']
-
 
 
 if __name__ == '__main__':
